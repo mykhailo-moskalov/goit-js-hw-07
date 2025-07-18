5f34f17852ef9cdef3e64713bfe5d1c8ad8b1ac2
mishokkartoshki <misha@moskalov.com>
2025-07-18 22:00:38 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+const input = document.querySelectorAll("input");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  const userData = {};
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  userData.email = email;
+  userData.password = password;
+
+  form.reset();
+
+  console.log(userData);
+}
